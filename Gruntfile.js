6ebfd79d3c739454620954e1b62e703945a3aac7
CJ Hanson <CJ.Hanson@rhythmagency.com>
2014-07-29 09:17:41 -0700

    Add option for skipping existing files when downloading

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,24 +1,27 @@
 /**
  * Usage:
- * $ grunt --bucket=NAME_OF_S3_BUCKET
+ * $ grunt download --bucket=NAME_OF_S3_BUCKET
+ *
+ * Optional:
+ * $ grunt download --bucket=NAME_OF_S3_BUCKET --overwrite=no
  */
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
-    'download-s3-bucket': {
-      options: {
-          bucket: grunt.option('bucket') || ''
-      },
-      build: {
-      }
-    }
-  });
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        'download-s3-bucket': {
+            options: {
+                bucket: grunt.option('bucket') || '',
+                overwrite: (grunt.option('overwrite') || 'yes').toLowerCase()
+            },
+            build: {
+            }
+        }
+    });
 
-  grunt.loadNpmTasks('grunt-contrib-download-s3-bucket');
+    grunt.loadNpmTasks('grunt-contrib-download-s3-bucket');
 
-  // Default task(s).
-  grunt.registerTask('default', ['download-s3-bucket']);
+    grunt.registerTask('download', ['download-s3-bucket']);
 
 };
