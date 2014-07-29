7f88efc44a0153ade737f89344caa057568c506c
CJ Hanson <CJ.Hanson@rhythmagency.com>
2014-07-29 09:28:24 -0700

    Prefer awsconfig.json but fall back to ~/.aws/credentials Updated usage example

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,6 +4,20 @@
  *
  * Optional:
  * $ grunt download --bucket=NAME_OF_S3_BUCKET --overwrite=yes
+ *
+ * This reads Amazon credentials from ./awsconfig.json and falls back to ~/.aws/credentials
+ *
+ * EXAMPLE awsconfig.json
+ {
+   "accessKeyId": "ACCESS_KEY_ID",
+   "secretAccessKey": "SECRET_ACCESS_KEY"
+ }
+ *
+ * EXAMPLE ~/.aws/credentials
+ [default]
+ aws_access_key_id = ACCESS_KEY_ID
+ aws_secret_access_key = SECRET_ACCESS_KEY
+ *
  */
 module.exports = function(grunt) {
 
